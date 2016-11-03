da318df1add860ec15f7304c1a831ab641a22bc2
Lalatendu Mohanty <LalatenduMohanty@users.noreply.github.com>
2016-11-03 14:31:35 +0530

    Removing the 1.3.0 origin download

--- ossetup.sh
+++ ossetup.sh
@@ -3,7 +3,6 @@
 yum install docker -y
 echo "INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'" >> /etc/sysconfig/docker
 systemctl restart docker
-wget https://github.com/openshift/origin/releases/download/v1.3.0/openshift-origin-client-tools-v1.3.0-3ab7af3d097b57f933eccef684a714f2368804e7-linux-64bit.tar.gz
 wget https://github.com/openshift/origin/releases/download/v1.3.1/openshift-origin-client-tools-v1.3.1-dad658de7465ba8a234a4fb40b5b446a45a4cee1-linux-64bit.tar.gz
 tar xvzf openshift-origin-client-tools-v1.3.1-dad658de7465ba8a234a4fb40b5b446a45a4cee1-linux-64bit.tar.gz
 ./openshift-origin-client-tools-v1.3.1-dad658de7465ba8a234a4fb40b5b446a45a4cee1-linux-64bit/oc cluster up
